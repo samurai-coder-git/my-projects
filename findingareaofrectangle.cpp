9334365a5f7a0e268d60802673d99054de92dd19
samurai-coder-git <shayanahmad20063@gmail.com>
2024-08-14 14:54:22 +0500

    Add files via upload

---
+++ findingareaofrectangle.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int length;
+    int width;
+    cout << " enter the length of rectangle \n";
+    cin >> length;
+    cout << " enter the width of rectangle \n";
+    cin >> width;
+    cout << "the area of ractangle is " << length * width;
+    return 0;
+}
